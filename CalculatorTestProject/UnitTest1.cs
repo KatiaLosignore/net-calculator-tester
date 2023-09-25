9c7eabac179043316e9e7b165737609c6f2b32f1
KatiaLosignore <katialos.kl@gmail.com>
2023-09-25 16:18:02 +0200

    added method TestSubstract

--- CalculatorTestProject/UnitTest1.cs
+++ CalculatorTestProject/UnitTest1.cs
@@ -1,10 +1,18 @@
 using net_calculator_tester;
+using Newtonsoft.Json.Bson;
 
 namespace CalculatorTestProject
 {
+    [TestFixture]
     public class Tests
     {
-  
+        Calculator calculator;
+
+        [SetUp]
+        public void Setup()
+        {
+            calculator = new Calculator();
+        }
 
         [Test]
         public void TestAdd()
@@ -17,13 +25,24 @@ namespace CalculatorTestProject
             float outputAtteso = 7.8f;
 
             // chiamo la funzione e salvo il risultato
-            float outputReale = Calculator.Add(x, y);
+            float outputReale = calculator.Add(x, y);
 
             // confronto output funzione con output atteso
             Assert.True(outputReale == outputAtteso, $"Il risultato atteso era {outputAtteso} mentre invece abbiamo {outputReale}");     
 
         }
 
-        
+
+        [Test]
+        [TestCase(4, 2, 2)]
+        [TestCase(2, -4, 8)]
+        [TestCase(2, -8, 3)]
+        public void TestSubstract(float x, float y, float result)
+        {
+            Assert.That(calculator.Subtract(x, y), Is.EqualTo(result));
+        }
+    
+
+
     }
 }
--- net-calculator-tester/Calculator.cs
+++ net-calculator-tester/Calculator.cs
@@ -6,23 +6,23 @@ using System.Threading.Tasks;
 
 namespace net_calculator_tester
 {
-    public static class Calculator
+    public class Calculator
     {
         // Metodo per la somma di due numeri float
-        public static float Add(float num1, float num2) 
+        public float Add(float num1, float num2) 
         {
             return num1 + num2; 
         }
 
         // Metodo per la sottrazione di due numeri float
-        public static float Subtract(float num1, float num2)
+        public float Subtract(float num1, float num2)
         {
             return num1 - num2;
         }
 
         // Metodo per la divisione di due numeri float
 
-        public static float Divide(float num1, float num2)
+        public float Divide(float num1, float num2)
         {
             if (num2 == 0)
             {
@@ -33,7 +33,7 @@ namespace net_calculator_tester
 
 
         // Metodo per la moltiplicazione di due numeri float
-        public static float Multiply(float num1, float num2)
+        public float Multiply(float num1, float num2)
         {
             return num1 * num2;
         }
