0da09bd2fd81e131a4d2204999ee694cb24bff01
KatiaLosignore <katialos.kl@gmail.com>
2023-09-25 15:34:32 +0200

    addded method Add

--- CalculatorTestProject/UnitTest1.cs
+++ CalculatorTestProject/UnitTest1.cs
@@ -1,16 +1,27 @@
+using net_calculator_tester;
+
 namespace CalculatorTestProject
 {
     public class Tests
     {
-        [SetUp]
-        public void Setup()
-        {
-        }
+  
 
         [Test]
-        public void Test1()
+        public void TestAdd()
         {
-            Assert.Pass();
+            // dichiaro input
+            float x = 4.30f;
+            float y = 3.50f;
+
+            // dichiaro output atteso
+            float outputAtteso = 7.8f;
+
+            // chiamo la funzione e salvo il risultato
+            float outputReale = Calculator.Add(x, y);
+
+            // confronto output funzione con output atteso
+            Assert.True(outputReale == outputAtteso, $"Il risultato atteso era {outputAtteso} mentre invece abbiamo {outputReale}");
+
         }
     }
 }
---
+++ net-calculator-tester/Calculator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace net_calculator_tester
+{
+    public static class Calculator
+    {
+        // Metodo per la somma di due numeri float
+        public static float Add(float num1, float num2) 
+        {
+            return num1 + num2; 
+        }
+
+
+    }
+}
