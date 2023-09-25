19e569f99e8543a7db5b2a1a88e9a5ae47f5e5e2
KatiaLosignore <katialos.kl@gmail.com>
2023-09-25 15:02:00 +0200

    Aggiungere i file di progetto.

---
+++ CalculatorTestProject/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace CalculatorTestProject
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
